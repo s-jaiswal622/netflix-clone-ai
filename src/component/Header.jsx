042fd18fdcf31c4e9a7b6e9a10bf70d774a3095a
Saloni Jaiswal <salonijaiswal622@gmail.com>
2025-06-08 21:23:39 +0530

    added header and image for netflix

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <div className="fixed px-8 py-4 w-full bg-gradient-to-b from-black">
+        <img
+            className="w-44"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="Netflix Logo Larger"
+        />
+      </div>
+    );
+  };
+  
+
+export default Header
---
+++ src/component/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+      <img
+        className="absolute top-0 left-0 w-full h-full"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/6863f6e8-d419-414d-b5b9-7ef657e67ce4/web/IN-en-20250602-TRIFECTA-perspective_27a3fdfa-126f-4148-b153-55d60b51be6a_medium.jpg"
+        alt="Background"
+      />
+        <Header />
+    </div>
+  );
+};
+export default Login;
