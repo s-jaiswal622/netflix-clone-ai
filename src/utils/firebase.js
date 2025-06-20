52d22133c719d5c37301c1dfa809ad6ac7891b25
Saloni Jaiswal <salonijaiswal622@gmail.com>
2025-06-20 15:28:09 +0530

    added secret key

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,35 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-const {
-  VITE_FIREBASE_API_KEY,
-  VITE_FIREBASE_AUTH_DOMAIN,
-  VITE_FIREBASE_PROJECT_ID,
-  VITE_FIREBASE_STORAGE_BUCKET,
-  VITE_FIREBASE_MESSAGING_SENDER_ID,
-  VITE_FIREBASE_APP_ID,
-} = import.meta.env;
 
-if (
-  !VITE_FIREBASE_API_KEY ||
-  !VITE_FIREBASE_AUTH_DOMAIN ||
-  !VITE_FIREBASE_PROJECT_ID ||
-  !VITE_FIREBASE_STORAGE_BUCKET ||
-  !VITE_FIREBASE_MESSAGING_SENDER_ID ||
-  !VITE_FIREBASE_APP_ID
-) {
-  throw new Error("Missing Firebase environment variables");
-}
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_API_KEY,
-  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: VITE_FIREBASE_PROJECT_ID,
-  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAHLarLTNnfvFe5IevxAcWatcOegwkFZqw",
+  authDomain: "movie-stream-clone.firebaseapp.com",
+  projectId: "movie-stream-clone",
+  storageBucket: "movie-stream-clone.firebasestorage.app",
+  messagingSenderId: "235266256813",
+  appId: "1:235266256813:web:cfd6bfcb3dbb6355603b24"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
